77e148661f55f5d36110ab4371a2a3b4c94e73d4
enginooby <enginooby@gmail.com>
2022-04-17 23:35:33 +0200

    Initial commit

---
+++ App.js
@@ -0,0 +1,17 @@
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+import HomeScreen from "./src/screens/HomeScreen";
+
+const navigator = createStackNavigator(
+  {
+    Home: HomeScreen,
+  },
+  {
+    initialRouteName: "Home",
+    defaultNavigationOptions: {
+      title: "Hello React Native",
+    },
+  }
+);
+
+export default createAppContainer(navigator);
